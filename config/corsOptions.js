09690262f46f740ed80df71f6452e1cdd80786a1
Ahmad Saulawa <ahmadlawal020@gmail.com>
2025-11-17 11:21:11 +0100

    second committt

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,14 +1,21 @@
-const allowedOrigins = require("./allowedOrigins");
+// const allowedOrigins = require("./allowedOrigins");
 
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
+
+// module.exports = corsOptions;
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: true, // allow all origins
+  credentials: true, // allow cookies/auth headers
   optionsSuccessStatus: 200,
 };
 
